f6d4979739f03003bb2c152c4c5373d962ea5fb7
Marcos Ferreira <xbrcvar@gmail.com>
2024-08-23 01:34:00 -0300

    adicionar hero da página sobre

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,30 @@
+import LinkButton from "../../components/button";
 import PageWrapper from "../../components/page-wrapper";
 
 export default function AboutPage() {
   return (
     <PageWrapper>
-      xx
+      <div className="hero">
+        <div className="hero__section">
+          <h1 className="hero__title">Bem-vindo à revolução dos vídeos!</h1>
+          <p className="hero__description">
+            Somos uma startup com o objetivo de facilitar a produção de conteúdo
+            em larga escala com qualidade profissional, capaz de atingir uma
+            audiência de milhões de seguidores.
+          </p>
+
+          <LinkButton to="#">Baixe o app</LinkButton>
+        </div>
+
+        <div className="hero__section">
+          <img
+            className="hero__image"
+            style={{ maxHeight: "80%" }}
+            src="mocks.svg"
+            alt="Capturas de tela do aplicativo"
+          />
+        </div>
+      </div>
     </PageWrapper>
-  )
+  );
 }
