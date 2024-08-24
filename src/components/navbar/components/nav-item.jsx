25ef0549572bf6b0d840ad6f0e9ea11c10b79edb
Marcos Ferreira <xbrcvar@gmail.com>
2024-08-24 17:46:37 -0300

    utilizar mergeClassname no navitem

--- src/components/navbar/components/nav-item.jsx
+++ src/components/navbar/components/nav-item.jsx
@@ -1,13 +1,12 @@
 import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
+import mc from "../../../utils/merge-classname";
 
 export default function NavItem(props) {
   return (
     <NavLink
       to={props.to}
-      className={({ isActive }) =>
-        isActive ? "navbar__nav-item active" : "navbar__nav-item"
-      }
+      className={({ isActive }) => mc("navbar__nav-item", isActive && "active")}
     >
       {props.children}
     </NavLink>
